3214c8544857515fda879e45437a2fe587c859c0
imannadeem825 <iman.nadeem.825@gmail.com>
2020-12-04 10:17:30 -0600

    exercise 4: map, filter, find

--- scripts/main.js
+++ scripts/main.js
@@ -41,23 +41,87 @@
 // Have her take that action by execucting the method you created.
 
 
-let painter = {
-  tools: ["brush", "roller", "caulk", "sandpaper", "dropcloth"],
-  uniform: "white overalls",
-  cost_per_hour: 25.00,
-  insured: true
+// let painter = {
+//   tools: ["brush", "roller", "caulk", "sandpaper", "dropcloth"],
+//   uniform: "white overalls",
+//   cost_per_hour: 25.00,
+//   insured: true
+// }
+
+// painter.paint = function () {
+//     return "brush stroke"
+// }
+
+// console.log(painter.paint())
+
+
+
+// Exercise 3
+// const button = document.querySelector(".btn--message");
+// const messageBox = document.querySelector(".message");
+// button.addEventListener("click", () => messageBox.innerHTML = "The button has been pressed!");
+
+
+
+
+// Exercise 4
+// Add a <section> element with an id of family to your index.html
+// Use a good old for loop to iterate over this array of objects to
+// pull out the names of the family members and represent them as HTML
+// by wrapping each in an <h3> tag. Append them to the DOM.
+// Achieve the same result using .map()
+
+let family = [
+  {
+    name: "Fred Jones",
+    age: 49,
+    title: "parent"
+  },
+  {
+    name: "Pat Jones",
+    age: 50,
+    title: "parent"
+  },
+  {
+    name: "Bubba Jones",
+    age: 20,
+    title: "adult child"
+  },
+  {
+    name: "Kelly Jones",
+    age: 12,
+    title: "dependent child"
+  },
+  {
+    name: "Bartleby",
+    age: 3,
+    title: "pet"
+  }
+]
+
+
+const familyEl = document.querySelector("#family")
+const familyNames = []
+for (let i=0; i < family.length; i++) {
+  familyNames.push(`<h3>${family[i].name}</h3>`)
 }
-
-painter.paint = function () {
-    return "brush stroke"
-}
-
-console.log(painter.paint())
+familyEl.innerHTML += familyNames.join("")
 
 
+// .map solution: shorthand of above
+familyEl.innerHTML = family.map( (famMember) => `<h3>${famMember.name}</h3>`).join("")
 
-// Exercise on events
+// extra: filter method
+const adults = family.filter( (familyMember) => {
+  if (familyMember.age >= 18) {
+    return true
+  }
+})
+console.log("grownups?", adults)
 
-const button = document.querySelector(".btn--message");
-const messageBox = document.querySelector(".message");
-button.addEventListener("click", () => messageBox.innerHTML = "The button has been pressed!");
+// shorthand for filter
+const adults = family.filter ( (familyMember) => familyMember.age >= 18)
+console.log("grownups?", adults)
+
+// find method--returns one object from the family array, the first time the condition is met, and then stops
+const parent = family.find( (familyMember) => familyMember.title === "parent")
